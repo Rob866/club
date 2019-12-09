9e93fe4f9f0260ef1adea26ac2938771be40f7d4
robert <juanrob_10@hotmail.com>
2019-12-08 21:26:11 -0700

    interfaz realizada

--- club/app/urls.py
+++ club/app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+app_name ='app'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('projects/', views.projects, name='projects'),
+    path('services/', views.services, name='services'),
+    path('testimony/', views.testimony, name='testimony'),
+    path('contact/', views.contact, name='contact'),
 ]
--- club/app/views.py
+++ club/app/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'app/index.html')
+def home(request):
+    return render(request, 'app/home.html')
+
+def about(request):
+    return render(request, 'app/about.html')
+
+def services(request):
+    return render(request, 'app/services.html')
+
+def projects(request):
+    return render(request, 'app/projects.html')
+
+def testimony(request):
+    return render(request, 'app/testimony.html')
+
+def contact(request):
+    return render(request,'app/contact.html')
